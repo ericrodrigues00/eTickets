1bdcfb69d308d51deb464dce1d5fdf6e39e67550
João Castro <jvinicius2002@gmail.com>
2023-09-03 21:41:37 -0300

    Adequação da encriptação de senha no cadastro e no login

--- ticketapp/database/user/userRegister.rb
+++ ticketapp/database/user/userRegister.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'mongo'
 require 'json'
-
+require 'bcrypt'
 
 # Configurar a conexão com o servidor MongoDB
 client = Mongo::Client.new('mongodb+srv://admin:adminadmin@cluster0.lcxcczi.mongodb.net/tex')
@@ -23,7 +23,6 @@ get '/users' do
   users.to_json
 end
 
-
 # Rota para receber os dados do formulário de registro em JSON
 post '/users' do
   request.body.rewind
@@ -33,15 +32,17 @@ post '/users' do
   email = data['email']
   cpf = data['cpf']
   password = data['password']
+  confirmPassword = data['confirmPassword']
 
-  
-  # hashed_password = BCrypt::Password.create(password)
+  # Criptografar a senha usando BCrypt
+  hashed_password = BCrypt::Password.create(password)
 
   doc_to_insert = {
     name: name,
     email: email,
     cpf: cpf,
-    password: password
+    password: hashed_password, # Use a senha criptografada aqui
+    confirmPassword: confirmPassword
   }
 
   result = users_collection.insert_one(doc_to_insert)
